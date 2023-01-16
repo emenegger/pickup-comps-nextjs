c99d3d5f6ef293c7d45d54b18a9d12a0412920fe
emenegger <evanemenegger@gmail.com>
2023-01-16 17:48:01 -0500

    started loading page functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,39 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import NavBar from "../src/components/NavBar";
 import { UserStatsProvider } from "../src/context/user-context";
-import { useState, Provider } from "react";
+import { useState, useEffect, Provider } from "react";
 import { AdjustedStatsProvider } from "../src/context/adjusted-stats-context";
 import { NbaCompProvider } from "../src/context/nba-comp-context";
 // import { UserProvider, useUser } from "@auth0/nextjs-auth0/client";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+
+// loading functionality
+function Loading() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  useEffect(()=> {
+    const handleStart = (url: string) => (url !== router.asPath) && setLoading(true);
+    const handleComplete = (url: string) => (url !== router.asPath) && setTimeout(()=> {setLoading(false)}, 5000);
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return ()=> {
+    router.events.off('routeChangeStart', handleStart);
+    router.events.off('routeChangeComplete', handleComplete);
+    router.events.off('routeChangeError', handleComplete);
+    }
+  })
+  return loading && (
+    <div className="spinner-wrapper">
+      <div className="spinner">Spinner</div>
+    </div>
+  )
+}
+
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
@@ -24,6 +49,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <AdjustedStatsProvider>
               <NbaCompProvider>
                 <NavBar />
+                {/* <Loading /> */}
                 <Component {...pageProps} />
               </NbaCompProvider>
             </AdjustedStatsProvider>
